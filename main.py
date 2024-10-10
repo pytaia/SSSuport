05596b442b4501e09fd082327f85815e80fd7b00
pytaia <pytaia@yandex.ru>
2024-10-10 03:05:29 +0300

    Added FAQ (added info)

--- main.py
+++ main.py
@@ -28,4 +28,23 @@ def start_place(message):
     bot.send_message(message.chat.id, 'Выберете интересующий вас раздел', reply_markup=markup)
 
 
+@bot.message_handler(func=lambda message: message.text in ['Курсы спортивного программирования', 'Курсы CTF',
+                                                           '"Продлёнка Айтишника"', 'Вступление в СНО ВГУ', 'Назад'])
+def simple_message_answer(message):
+    if message.text == 'Курсы спортивного программирования':
+        bot.send_message(message.chat.id, "Курсы спортивного программирования? А, эм... "
+                                          "К сожалению в разработке бота не принимали участия представителя этого "
+                                          "направления, все вопросы к ним")
+    elif message.text == '"Продлёнка Айтишника"':
+        bot.send_message(message.chat.id, "Ну этого когда матёрые програмисты котяры расссказывают студентам "
+                                          "котятам как им ходить в ... С чем им предстоит столкнуться на работе по "
+                                          "специальности. Все же выпускники ФКН будут работать по специальности?")
+    elif message.text == 'Вступление в СНО ВГУ':
+        bot.send_message(message.chat.id, "Вот если сделаешь нам пару тасков ...")
+    elif message.text == 'Курсы CTF':
+        bot.send_message(message.chat.id, 'Вместо 1000 слов, оть чат ТГ @vrnCTFCourses')
+    elif message.text == 'Назад':
+        start_place(message)
+
+
 bot.polling(none_stop=True, interval=0)
