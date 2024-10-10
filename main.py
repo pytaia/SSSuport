765fd868aa4573ca7f125629bc0ff8bcfd53c96a
pytaia <pytaia@yandex.ru>
2024-10-10 03:06:19 +0300

    Added CTF info (learn more ...)

--- main.py
+++ main.py
@@ -47,4 +47,57 @@ def simple_message_answer(message):
         start_place(message)
 
 
+@bot.message_handler(func=lambda message: message.text == "Что такое CTF")
+def ctf_info(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton('Steganography'), types.KeyboardButton('WEB'), types.KeyboardButton('OSINT'),
+               types.KeyboardButton('Reverse'), types.KeyboardButton('Cryptography'),
+               types.KeyboardButton('Forensic'), types.KeyboardButton('Назад'))
+
+    bot.send_message(message.chat.id, "Захват флага (англ. Capture the Flag или англ. CTF) в компьютерной безопасности "
+                                      "— это упражнение, в котором «флаги» тайно прячутся в преднамеренно "
+                                      "уязвимых программах или веб-сайтах. (взято с Википедии, а больше мы ничего и "
+                                      "не знаем, крч вот чат @vrnCTFCourses)\n"
+                                      "Выберите интересующую вас категорию заданий",
+                     reply_markup=markup)
+
+
+@bot.message_handler(func=lambda message: message.text in ['Steganography', 'WEB', 'OSINT', 'Reverse',
+                                                           'Cryptography', 'Forensic', 'Назад'])
+def ctf_task_category_info(message):
+    if message.text == 'Steganography':
+        bot.send_message(message.chat.id,
+                         "Steganography (Стеганография) -  практика сокрытия сообщения, файла или информации "
+                         "внутри других несекретных данных или носителя, таких как изображение, "
+                         "аудиофайл или текст, таким образом, чтобы существование этих данных было скрыто.")
+    elif message.text == 'OSINT':
+        # Жирная подсказка куда надо дальше двигаться что бы получить флаг
+        # Но можно дать ссылку на репозиторий в раделе OSINT чата Курсы CTF, просто поменять немного ответы бота
+        # Участников нагнать заодно)
+        bot.send_message(message.chat.id,
+                         "OSINT (Поиск информации в открытых источниках) Поиск, выбор и сбор информации из "
+                         "общедоступных источников, а также её анализ и систематизацию. Анализ данных из проектов "
+                         "в Github, как часть Open Source разработки тоже можно считать частью OSINT, код "
+                         "этого бота также представлен на Github (pytaia/SSSuport)")
+    elif message.text == 'WEB':
+        bot.send_message(message.chat.id,
+                         "Web (Уязвимости веб-приложений, атака на веб-приложения) \n По больше части в "
+                         "заданиях вам даётся адрес сайта, который нужно 'поломать' с помощью уязвимости. "
+                         "Часто, на различных простых CTF'ах задания бывают просто на знание "
+                         "устройства Веб-приложений.")
+    elif message.text == 'Reverse':
+        bot.send_message(message.chat.id,
+                         'Reverse (обратная разработка) \n Исследование бинарных файлов (программ) без '
+                         'исходных кодов и изучение работы различных редких архитектур.')
+    elif message.text == 'Cryptography':
+        bot.send_message(message.chat.id, 'Cryptography (криптография) Задания на криптографические алгоритмы. Бывают '
+                                          'задания как на старинные алгоритмы, так и на современные.')
+    elif message.text == 'Forensic':
+        bot.send_message(message.chat.id, 'Forensic (компьютерная криминалистика) Задания посвящённые компьютерной '
+                                          'криминалистики. В данную тематику входит: исследование различных дампов '
+                                          '(сетевых, памяти и прочее), восстановление архивов.')
+    else:
+        start_place(message)
+
+
 bot.polling(none_stop=True, interval=0)
